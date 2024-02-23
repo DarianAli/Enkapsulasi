fa5c48f0095da06b5bbe25bfdac0fc62b5e23bd0
DarianAli <143059246+DarianAli@users.noreply.github.com>
2024-02-23 10:06:33 +0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.user;
+
+/**
+ *
+ * @author ASUS
+ */
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
